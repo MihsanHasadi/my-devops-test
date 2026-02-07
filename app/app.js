b3bebaa2ca666727e997540231b10f5dec71545d
Ubuntu <azureuser@VM-2.2kz3wchp3hoelhgd15qtam3ndc.hx.internal.cloudapp.net>
2026-02-07 20:08:57 +0000

    Initial Node.js app

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello DevOps Hybrid Deployment 🚀</h1><p>Status: Running on ' + process.platform + '</p>');
+  res.send('<h1>Hello DevOps Hybrid Deployment </h1><p>Status: Running on ' + process.platform + '</p>');
 });
 
 app.listen(port, () => {
